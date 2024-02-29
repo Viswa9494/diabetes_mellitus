3810619303bc1eb163960683c0c9fc5b86b5fd92
Viswa9494 <87115305+Viswa9494@users.noreply.github.com>
2024-02-29 15:36:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request,jsonify
+import pickle
+import numpy as np
+
+f = open('diabetmodel.pkl', 'rb')
+
+
+model = pickle.load(f)
+
+
+f.close()
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hello World"
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    glucose=request.form.get('glucose')
+    bloodPressure=request.form.get('bloodPressure')
+    skinThickness=request.form.get('skinThickness')
+    insulin=request.form.get('insulin')
+    bmi=request.form.get('bmi')
+    age=request.form.get('age')
+
+    input_query=np.array([[glucose,bloodPressure,skinThickness,insulin,bmi,age]])
+    result=model.predict(input_query)[0]
+    #result={'glucose':glucose,'bloodPressure':bloodPressure,'skinThickness':skinThickness,'insulin':insulin,'bmi':bmi,'dpf':dpf,'age':age}
+    return jsonify({'risk_level':str(result)})
+
+if __name__=='__main__':
+    app.run(debug=True)
